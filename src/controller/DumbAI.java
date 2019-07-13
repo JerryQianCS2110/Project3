2b51b285683ccb6f34ad43dedf05dc78d8b18047
Jerry Qian <jsq5@cornell.edu>
2019-07-12 20:58:51 -0400

    Added DumbAI nextMove method

--- src/controller/DumbAI.java
+++ src/controller/DumbAI.java
@@ -1,6 +1,7 @@
 package controller;
 
 import model.Game;
+import model.Board;
 import model.Location;
 import model.NotImplementedException;
 import model.Player;
@@ -14,14 +15,44 @@ public class DumbAI extends Controller {
 	public DumbAI(Player me) {
 		super(me);
 		// TODO Auto-generated constructor stub
-		throw new NotImplementedException();
+		//throw new NotImplementedException();
+		
+		
 	}
 
+	/**
+	 * Return the Location of the next move this player should make in
+	 * game g, or null if the player cannot play.
+	 * 
+	 * Precondition, it is this player's turn.
+	 */
 	protected @Override Location nextMove(Game g) {
 		// Note: Calling delay here will make the CLUI work a little more
 		// nicely when competing different AIs against each other.
 		
 		// TODO Auto-generated method stub
-		throw new NotImplementedException();
+		//throw new NotImplementedException();
+		
+		int emptyRow = -1;
+		int emptyCol = -1;
+		
+		Board currentBoard = g.getBoard();
+				
+		for(int r = 0; r < 9; r += 1) {
+			for(int c = 0; c < 9; c += 1) {
+				if(currentBoard.get(r, c) == null) {
+					emptyRow = r;
+					emptyCol = c;
+					break;
+				}
+			}
+		}
+		
+		if(emptyRow != -1 && emptyCol != -1) {
+			Location nextMove = new Location(emptyRow, emptyCol);		
+			return nextMove;
+		} else {
+			return null;
+		}
 	}
 }
